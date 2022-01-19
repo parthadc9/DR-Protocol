9f32c7583400e96291e3e309ffc73b6c006577a9
parthadc9 <partha.daschowdhury@bristol.ac.uk>
2022-01-19 15:34:09 +0000

    First Version

---
+++ DR-Protocol/set-user.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# This is a collection of bash functions used by different scripts
+
+# imports
+. scripts/set-user-global.sh
+. scripts/utils.sh
+
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/configtx
+export VERBOSE=false
+FABRIC_CFG_PATH=$PWD/../config/
+
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export PEER0_ORG3_CA=${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
+
+ORG=$1
+USER=$2
+
+setGlobals $ORG $USER
+
+#peer chaincode query -C drchannel -n payment -c '{"Args":["GetPayee", "123", "423"]}'
